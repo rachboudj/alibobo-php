cbbb9c6e668e02f0b985fb743026779b7d384036
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-06-27 15:33:41 +0200

    ajout du fuseau horaire

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 
+// Définir le fuseau horaire dans lequel le serveur se trouve 
+date_default_timezone_set('Europe/Paris');
+
 include './includes/header.php';
 include './includes/main.php';
 include './includes/footer.php';
