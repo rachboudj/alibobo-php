7906bf71990d5ff517ff310652c183d423cb6542
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-04 14:13:45 +0200

    suppression du formulaire dans la page d'accueil

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,11 +1,1 @@
 <h1>Accueil</h1>
-
-<form action="">
-        <label for="">Nom :</label>
-        <input type="text" name="" id="">
-        <label for="">Prénom :</label>
-        <input type="text" name="" id="">
-        <label for="">Mail :</label>
-        <input type="text" name="" id="">
-        <button type="submit">Soumettre</button>
-    </form>
