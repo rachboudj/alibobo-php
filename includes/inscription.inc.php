6bc2fbea4d1b859376805376ed1e7cbbe0ac880d
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-04 15:00:42 +0200

    Opérateurs ternaires pour récupérer des superglobales de la fonction trim()

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -2,10 +2,19 @@
 
 <?php 
 
-if (isset($_POST['frmInsccription'])) {
+if (isset($_POST['frmInscription'])) {
     echo "Je viens du formulaire";
+
+    $nom = isset($_POST['nom']) ? trim($_POST['nom']) : "";
+    $prenom = isset($_POST['prenom']) ? trim($_POST['prenom']) : "";
+    $email = isset($_POST['email']) ? trim($_POST['email']) : "";
+    $mdp1 = isset($_POST['mdp1']) ? trim($_POST['mdp1']) : "";
+    $mdp2 = isset($_POST['mdp2']) ? trim($_POST['mdp2']) : "";
+
 } else {
     echo "Je viens du futur";
 }
+
+// dumb($_POST);
 
 require_once 'frmInscription.php';
