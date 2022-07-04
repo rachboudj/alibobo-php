6205b4b8cc6dc8e4c237ad666633a4f78b307faa
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-04 14:35:37 +0200

    création d'un formulaire d'inscription

---
+++ includes/frmInscription.php
@@ -0,0 +1,26 @@
+<form action="index.php?page=inscription" method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" />
+    </div>
+    <div>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" />
+    </div>
+    <div>
+        <label for="email">Email :</label>
+        <input type="text" id="email" name="email" />
+    </div>
+    <div>
+        <label for="mdp1">Mot de passe :</label>
+        <input type="password" id="password" name="password" />
+    </div>
+    <div>
+        <label for="mdp2">Nom :</label>
+        <input type="text" id="nom" name="nom" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Soumettre" />
+    </div>
+</form>
