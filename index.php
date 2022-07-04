ce6b57c40635b622da2b0a4cc0e114d781e3cb84
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-04 09:40:07 +0200

    affichage de la fonction

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
 
+// echo "<pre>";
+// var_dump($_SERVER);
+// echo "</pre>";
+
+echo $_SERVER["REQUEST_URI"];
+
 // Définir le fuseau horaire dans lequel le serveur se trouve 
 date_default_timezone_set('Europe/Paris');
 
