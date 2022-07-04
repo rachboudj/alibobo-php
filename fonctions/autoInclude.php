b805738215aed4e6c61db511d4791053cab819a2
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-04 12:35:03 +0200

    modif : la fonction redirige vers la page d'accueil qd la page n'existe pas ou qu'il y en a pas

--- fonctions/autoInclude.php
+++ fonctions/autoInclude.php
@@ -1,7 +1,14 @@
 <?php 
 
-function autoInclude($page) {
-    require_once './includes/' .$page . ".inc.php";
+function autoInclude(string $file): void {
+    $includedFiles = glob("./includes/*.inc.php");
+    $file = "./includes/" . $file . ".inc.php";
+
+    if (count($includedFiles) !== 0 && in_array($file, $includedFiles)) {
+        require_once $file;
+    } else {
+        require_once './includes/accueil.inc.php';
+    }
 }
 
 
