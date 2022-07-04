22c3ee7a36901e028c857cc9aa1183e0cb2a1242
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-04 16:06:47 +0200

    ajout d'un message d'erreur

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -11,10 +11,27 @@ if (isset($_POST['frmInscription'])) {
     $mdp1 = isset($_POST['mdp1']) ? htmlentities(trim($_POST['mdp1'])) : "";
     $mdp2 = isset($_POST['mdp2']) ? htmlentities(trim($_POST['mdp2'])) : "";
 
+    $erreurs = array();
+
+    if (mb_strlen($nom) === 0)
+        array_push($erreurs, "Veuillez saisir votre nom");
+    
+    if (mb_strlen($prenom) === 0)
+        array_push($erreurs, "Veuillez saisir votre prénom");
+    
+    if (mb_strlen($email) === 0)
+        array_push($erreurs, "Veuillez saisir une adresse mail");
+
+    $messageErreurs = "<ul>";
+    for ($i = 0; $i < count($erreurs); $i++) {
+        $messageErreurs .= "<li>";
+        $messageErreurs .= $erreurs[$i];
+        $messageErreurs .= "<li>";
+    }
+
+    $messageErreurs .= "</ul>";
+
 } else {
+    require_once 'frmInscription.php';
     echo "Je viens du futur";
 }
-
-// dumb($_POST);
-
-require_once 'frmInscription.php';
