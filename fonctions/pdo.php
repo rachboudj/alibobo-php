2dad19d90fe186dcfb16aa11be969d27c001dc04
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-12 22:57:54 +0200

    ajout d'une fonction dans pdo

--- fonctions/pdo.php
+++ fonctions/pdo.php
@@ -1,11 +1,16 @@
 <?php
 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=alibobo', "root", "root", array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
-    ));
-} catch (PDOException $e) {
-    echo 'Erreur de connexion : ' . $e->getMessage();
+function pdo()
+{
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=alibobo', "root", "root", array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        ));
+        return $pdo;
+    } catch (PDOException $error) {
+        echo 'Erreur de connexion : ' . $error->getMessage();
+        return false;
+    }
 }
