92c9690bc48cfd611f85a1153496d2f7fd54ddbb
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-15 17:32:13 +0200

    correction des erreurs, affichage des produits par ordre croissant

--- includes/articles.inc.php
+++ includes/articles.inc.php
@@ -1,21 +1,21 @@
 <?php 
 
-$sql = "SELECT * FROM articles ORDER BY created_at ASC";
+$pdo = pdo();
+$sql = "SELECT * FROM articles ORDER BY id_article ASC";
 $query = $pdo->prepare($sql);
 $query->execute();
 $alibobos = $query->fetchAll();
 
-include './includes/header.php';
 ?>
 
 <h1>Les produits Alibobo</h1>
-    <a href="new-beer.php">Ajouter un produit</a>
+    <a href="nouvelleArticles.php">Ajouter un produit</a>
     <ul>
         <?php foreach($alibobos as $alibobo) {
-            debug($alibobo); ?>
+            //debug($alibobo); ?>
             <li>
                 <h2><?php echo ucfirst($alibobo['designation']); ?></h2>
-                <a href="detailProduit.php?id=<?php echo $alibobo['id']; ?>">Voir plus</a>
+                <a href="detailProduit.php?id=<?php echo $alibobo['id_article']; ?>">Voir plus</a>
             </li>
         <?php } ?>
     </ul>
