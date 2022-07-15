a0360a7157325659b3f5112d0d9376fd344a4a79
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-15 17:22:08 +0200

    transfère du listing des produits dans articles

--- includes/articles.inc.php
+++ includes/articles.inc.php
@@ -1,1 +1,24 @@
-<h1>Articles</h1>
+<?php 
+
+$sql = "SELECT * FROM articles ORDER BY created_at ASC";
+$query = $pdo->prepare($sql);
+$query->execute();
+$alibobos = $query->fetchAll();
+
+include './includes/header.php';
+?>
+
+<h1>Les produits Alibobo</h1>
+    <a href="new-beer.php">Ajouter un produit</a>
+    <ul>
+        <?php foreach($alibobos as $alibobo) {
+            debug($alibobo); ?>
+            <li>
+                <h2><?php echo ucfirst($alibobo['designation']); ?></h2>
+                <a href="detailProduit.php?id=<?php echo $alibobo['id']; ?>">Voir plus</a>
+            </li>
+        <?php } ?>
+    </ul>
+
+<?php 
+include './includes/footer.php';
