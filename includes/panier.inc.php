9ff6ce2d4b520db257d66d2fdfea2eeb5ecf0a09
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-17 12:02:19 +0200

    ajout de la structure html du panier

--- includes/panier.inc.php
+++ includes/panier.inc.php
@@ -4,3 +4,16 @@
 
 <h1>Votre panier</h1>
 
+<div class="container-cart">
+    <div class="card-cart">
+        <div class="title-price-cart">
+            <h2>Pomme Golden</h2>
+            <p class="price">12.99</p>
+        </div>
+        <p class="status">En stock</p>
+        <div class="quantity-delete-cart">
+            <input type="number" name="quantity" id="quantity">
+            <button><a href="">Supprimer</a></button>
+        </div>
+    </div>
+</div>
