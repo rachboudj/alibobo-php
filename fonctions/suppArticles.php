4dd62c9651541ad9d25aa1a8eaa3aefe3fc9926e
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-21 10:00:26 +0200

    correction de l'attribut id dans le

--- fonctions/suppArticles.php
+++ fonctions/suppArticles.php
@@ -1,8 +1,8 @@
 <?php 
 
 function suppArticle () {
-    if(!empty($_GET['id']) && ctype_digit($_GET['id'])) {
-        $articleId = $_GET['id'];
+    if(!empty($_GET['articleId']) && ctype_digit($_GET['articleId'])) {
+        $articleId = $_GET['articleId'];
         if ($pdo = pdo()) {
             $sql = "DELETE FROM articles WHERE articles.id_article = '$articleId'";
             $reponse = $pdo->query($sql);
